2612fe63188ab358afac67f7147264439800d8ef
danietsuzuki <massao.tsuzuki@gmail.com>
2023-05-04 18:22:08 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/estudo/alurachallengebackendsemana1/AluraChallengeBackendSemana1Application.java
@@ -0,0 +1,13 @@
+package br.com.estudo.alurachallengebackendsemana1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallengeBackendSemana1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraChallengeBackendSemana1Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/estudo/alurachallengebackendsemana1/AluraChallengeBackendSemana1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.alurachallengebackendsemana1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallengeBackendSemana1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
