47a7ed20727a809a5fb3ae88d08939ec3bc3e1d4
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-05-19 20:55:10 -0300

    Method findByTitle using Query parameters

--- src/main/java/br/com/estudo/alurachallengebackendsemana1/repositories/VideoRepository.java
+++ src/main/java/br/com/estudo/alurachallengebackendsemana1/repositories/VideoRepository.java
@@ -3,6 +3,7 @@ package br.com.estudo.alurachallengebackendsemana1.repositories;
 import br.com.estudo.alurachallengebackendsemana1.domain.entities.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +13,7 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
     Optional<Video> findByIdAndActiveTrue(Long id);
     @Query()
     List<Video> findAllByActiveTrue();
+
+    @Query(value = "SELECT * FROM videos WHERE title LIKE %:title% ",nativeQuery = true)
+    List<Video> findByTitleContaining(@Param("title") String title);
 }
