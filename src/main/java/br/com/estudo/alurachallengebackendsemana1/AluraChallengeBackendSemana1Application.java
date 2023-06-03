18a49add2c707f0dfcbbd5f0d03463955f1a0034
danietsuzuki <massao.tsuzuki@gmail.com>
2023-06-03 16:06:22 -0300

    activate cors

--- src/main/java/br/com/estudo/alurachallengebackendsemana1/AluraChallengeBackendSemana1Application.java
+++ src/main/java/br/com/estudo/alurachallengebackendsemana1/AluraChallengeBackendSemana1Application.java
@@ -1,10 +1,12 @@
 package br.com.estudo.alurachallengebackendsemana1;
 
+import br.com.estudo.alurachallengebackendsemana1.utils.security.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-//@Import(CorsConfig.class)
+@Import(CorsConfig.class)
 public class AluraChallengeBackendSemana1Application {
 
     public static void main(String[] args) {
--- src/main/java/br/com/estudo/alurachallengebackendsemana1/utils/security/CorsConfig.java
+++ src/main/java/br/com/estudo/alurachallengebackendsemana1/utils/security/CorsConfig.java
@@ -1,18 +1,18 @@
-//package br.com.estudo.alurachallengebackendsemana1.utils.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("*")
-//                .allowedHeaders("*");
-//    }
-//}
+package br.com.estudo.alurachallengebackendsemana1.utils.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
